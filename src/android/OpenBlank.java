484cb832d6b31e7a5ff447639cdd2016c2b4a7f6
JacobEdmondKerr <44099288+JacobEdmondKerr@users.noreply.github.com>
2020-01-03 14:57:43 -0700

    Update OpenBlank.java

--- src/android/OpenBlank.java
+++ src/android/OpenBlank.java
@@ -1,17 +1,58 @@
 package org.apache.cordova.openblank;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.Browser;
-import android.util.Log;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaHttpAuthHandler;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.PluginResult;
+import android.annotation.SuppressLint;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Browser;
+
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.HttpAuthHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 
 @SuppressLint("SetJavaScriptEnabled")
-public boolean onOverrideUrlLoading(String url) {
-    	// Log.d("OpenBlank", "onOverrideUrlLoading called with URL " + url);
-   		//if(url.indexOf("utm_") > -1 || url.indexOf("adurl") > -1) {
+public class OpenBlank extends CordovaPlugin {
+    @Override
+    public boolean onOverrideUrlLoading(String url) {
+    	Log.d("OpenBlank", "onOverrideUrlLoading called with URL " + url);
+   		if(url.indexOf("google") > -1 || url.indexOf(".com") > -1 || url.indexOf(".net") > -1 || url.indexOf(".org") > -1) {
 
 	        try {
 	            Intent intent = null;
@@ -28,10 +69,11 @@ public boolean onOverrideUrlLoading(String url) {
 	            this.cordova.getActivity().startActivity(intent);
 	            return true;
 	        } catch (android.content.ActivityNotFoundException e) {
-	            // Log.d("OpenBlank", "OpenBlank: Error loading url "+url+":"+ e.toString());
+	            Log.d("OpenBlank", "OpenBlank: Error loading url "+url+":"+ e.toString());
 	            return false;
 	        }
-   		//}
+   		}
 
    		return false;
     }
+}
