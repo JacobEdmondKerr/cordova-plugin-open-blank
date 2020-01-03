4b69668ddfff2eafe8c89c83a383883f0a5c2eab
JacobEdmondKerr <44099288+JacobEdmondKerr@users.noreply.github.com>
2020-01-03 14:24:14 -0700

    Update OpenBlank.java
    Removed webview. sendJavascript for just sendJavascript (I guess webview. sendJavascript is depreciated)

--- src/android/OpenBlank.java
+++ src/android/OpenBlank.java
@@ -19,7 +19,7 @@ public class OpenBlank extends CordovaPlugin {
             // Adding the MIME type to http: URLs causes them to not be handled by the downloader.
             Uri uri = Uri.parse(url);
             if ("http".equals(uri.getScheme()) || "https".equals(uri.getScheme())) {
-                webView.sendJavascript("cordova.InAppBrowser.open('" + url + "', '_blank');");
+                sendJavascript("cordova.InAppBrowser.open('" + url + "', '_blank');");
             } else {
                 return false;
             }
