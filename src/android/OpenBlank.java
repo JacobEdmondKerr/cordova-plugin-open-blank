68e9924c11f2a5e2f56578917fa09b2e0aef9151
JacobEdmondKerr <44099288+JacobEdmondKerr@users.noreply.github.com>
2020-01-03 14:56:17 -0700

    Update OpenBlank.java

--- src/android/OpenBlank.java
+++ src/android/OpenBlank.java
@@ -9,25 +9,29 @@ import org.apache.cordova.CordovaPlugin;
 
 
 @SuppressLint("SetJavaScriptEnabled")
-public class OpenBlank extends CordovaPlugin {
-    @Override
-    public boolean onOverrideUrlLoading(String url) {
-        Log.d("OpenBlank", "onOverrideUrlLoading called with URL " + url);
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            // Omitting the MIME type for file: URLs causes "No Activity found to handle Intent".
-            // Adding the MIME type to http: URLs causes them to not be handled by the downloader.
-            Uri uri = Uri.parse(url);
-            if ("http".equals(uri.getScheme()) || "https".equals(uri.getScheme())) {
-                //this.cordova.InAppBrowser.open('" + url + "', '_blank');
-                webView.sendJavascript("cordova.InAppBrowser.open('" + url + "', '_blank');");
-            } else {
-                return false;
-            }
-            return true; // true prevents navigation navigation
-        } catch (android.content.ActivityNotFoundException e) {
-            Log.d("OpenBlank", "OpenBlank: Error loading url " + url + ":" + e.toString());
-            return false;
-        }
+public boolean onOverrideUrlLoading(String url) {
+    	// Log.d("OpenBlank", "onOverrideUrlLoading called with URL " + url);
+   		//if(url.indexOf("utm_") > -1 || url.indexOf("adurl") > -1) {
+
+	        try {
+	            Intent intent = null;
+	            intent = new Intent(Intent.ACTION_VIEW);
+	            // Omitting the MIME type for file: URLs causes "No Activity found to handle Intent".
+	            // Adding the MIME type to http: URLs causes them to not be handled by the downloader.
+	            Uri uri = Uri.parse(url);
+	            if ("file".equals(uri.getScheme())) {
+	                intent.setDataAndType(uri, webView.getResourceApi().getMimeType(uri));
+	            } else {
+	                intent.setData(uri);
+	            }
+	            intent.putExtra(Browser.EXTRA_APPLICATION_ID, cordova.getActivity().getPackageName());
+	            this.cordova.getActivity().startActivity(intent);
+	            return true;
+	        } catch (android.content.ActivityNotFoundException e) {
+	            // Log.d("OpenBlank", "OpenBlank: Error loading url "+url+":"+ e.toString());
+	            return false;
+	        }
+   		//}
+
+   		return false;
     }
-}
